8df96c8ce3d636145f9d2f411f557a181bd1dce9
Roberto <robertooliveiraestudo@gmail.com>
2016-05-27 23:15:01 -0300

    tuto 8 of 20

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,6 +1,5 @@
 import { createStore, compose } from 'redux';
-import { syncHistoryWithStore } from 'react-store-redux';
-
+import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
 // Import the root reducer
